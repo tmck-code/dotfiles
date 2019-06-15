772425e2f681c935c19dd7840d4275a297b91920
Tom McKeesick <tmck01@gmail.com>
2019-06-15 16:18:50 +1000

    Adjust log format

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 
 if [ -z "${1:-}" ]; then
-  echo "Must provide an OS as \$1"
+  echo "- Must provide an OS as \$1"
   exit 1
 fi
 
 OS="${1}"
-echo "Installing dotfiles for OS: '${OS}'"
+echo "- Installing dotfiles for OS: '${OS}'"
 
 mv -v $HOME/.bashrc $HOME/.bashrc.bak || echo "- No bashrc file found! Nothing to back up here"
 
@@ -31,18 +31,21 @@ kitty.light-gruvbox.conf
 "
 
 function install_general() {
+  echo "- Installing 'general' dotfiles"
   for d in $(echo "${general_dotfiles}"); do
     ln -svf "$(pwd)/general/${d}" "$HOME/.${d}"
   done
 }
 
 function install_linux() {
+  echo "- Installing linux dotfiles"
   for d in $(echo "${linux_dotfiles}"); do
     ln -svf "$(pwd)/linux/${d}" "$HOME/.${d}"
   done
 }
 
 function install_osx() {
+  echo "- Installing osx dotfiles"
   for d in $(echo "${osx_dotfiles}"); do
     ln -svf "$(pwd)/osx/${d}" "$HOME/.${d}"
   done
