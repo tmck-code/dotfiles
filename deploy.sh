ab189a016d0dec646f207466f87a39992e33a474
Tom McKeesick <tmck01@gmail.com>
2019-06-15 16:07:52 +1000

    Bugfix for linking files

--- deploy.sh
+++ deploy.sh
@@ -8,47 +8,43 @@ if [ -z "${1:-}" ]; then
 fi
 
 OS="${1}"
+echo "Installing dotfiles for OS: '${OS}'"
 
 mv -v $HOME/.bashrc $HOME/.bashrc.bak || echo "- No bashrc file found! Nothing to back up here"
 
-general_dotfiles=$(cat <<EOF
+general_dotfiles="
 gitconfig
 rubocop.yml
 tmux.conf
-vimrc
-EOF
-)
+vimrc"
 
-linux_dotfiles=$(cat <<EOF
+linux_dotfiles="
 bash_aliases
 bash_profile
 bashrc
 inputrc
-tmux.conf
-EOF
-)
+tmux.conf"
 
-osx_kitty_dotfiles=$(cat <<EOF
+osx_kitty_dotfiles="
 kitty.conf
 kitty.light-gruvbox.conf
-EOF
-)
+"
 
 function install_general() {
-  for d in "$(echo ${general_dotfiles[@]})"; do
-    ln -svf "$(pwd)/general/${d}"
+  for d in $(echo "${general_dotfiles}"); do
+    ln -svf "$(pwd)/general/${d}" "$HOME/.${d}"
   done
 }
 
 function install_linux() {
-  for d in "$(echo ${linux_dotfiles[@]})"; do
-    ln -svf "$(pwd)/linux/${d}"
+  for d in $(echo "${linux_dotfiles}"); do
+    ln -svf "$(pwd)/linux/${d}" "$HOME/.${d}"
   done
 }
 
 function install_osx() {
-  for d in "$(echo ${osx_dotfiles[@]})"; do
-    ln -svf "$(pwd)/osx/${d}"
+  for d in $(echo "${osx_dotfiles}"); do
+    ln -svf "$(pwd)/osx/${d}" "$HOME/.${d}"
   done
 }
 
